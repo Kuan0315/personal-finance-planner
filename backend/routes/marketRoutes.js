6680090a0253bde9b37a3e38e2bb25d44bb58c05
HZ Tan <zhe.ht0623@gmail.com>
2026-05-09 12:26:42 +0800

    Remove backend directory from frontend only branch

--- backend/routes/marketRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getMarketInsights } = require('../controllers/marketController.js');
-
-router.get('/', getMarketInsights);
-
-module.exports = router;
